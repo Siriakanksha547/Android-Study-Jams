f8d484fbe5cd317e5d699f5ee5cf30a23b0a1efb
Siriakanksha547 <75832212+Siriakanksha547@users.noreply.github.com>
2020-12-18 20:09:53 +0530

    Create q2.kt

---
+++ week1/q2.kt
@@ -0,0 +1,18 @@
+
+fun main(){
+    val a=3
+    val b=2
+    val c=1
+    if(a>b){
+        if(a>c)
+        print("a is the largest")
+        else
+        print("c is the largest")
+    }
+    else{
+        if(b>c)
+        print("b is the largest")
+        else
+        print("c is the largest")
+    }
+}
